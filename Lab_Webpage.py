45abbf5d26dfdb1816599042c731a8062e1bc687
junglezone56 <90592347+junglezone56@users.noreply.github.com>
2023-11-07 13:43:36 -0600

    Add files via upload

---
+++ Lab_Webpage.py
@@ -0,0 +1,26 @@
+from tkinter import N
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+#Make a homepage
+@app.route('/')
+def homepage():
+    
+
+@app.route('/hello/<name>')
+def hello(name):
+    listofNames = [name, "Johnny", "Enigma"]
+    return render_template('HTMLPage1.html',Sname = name, nameList = listofNames)
+
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo(name=None:)
+    if request.method == 'POST':
+        name=request.form['name']
+   return render_template('form.html', name=name)
+
+
+#Add the option to run this file directly
+if __name__ == "__main__":
+    app.run(debug = True)
